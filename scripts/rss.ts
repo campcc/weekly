4054497e279ea4fd26875211a9dd99a81910db5e
lishangfeng02 <lishangfeng02@meituan.com>
2023-11-22 18:44:51 +0800

    No.001

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -46,16 +46,13 @@ posts.forEach((post) => {
     author: author.name,
     date: post.date,
     custom_elements: [
-      { 'itunes:author': 'John Doe' },
-      { 'itunes:subtitle': 'A short primer on table spices' },
       {
-        'itunes:image': {
+        image: {
           _attr: {
             href: 'https://img.alicdn.com/imgextra/i2/O1CN013IYhe31M3AgB9FQll_!!6000000001378-0-tps-1920-1280.jpg_1200x1200.jpg',
           },
         },
       },
-      { 'itunes:duration': '7:04' },
     ],
   });
 });
