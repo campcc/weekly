1e713cc6de8783ba613e6b9c2f7e17f5b80e886c
Monch Lee~ <lishangfeng02@meituan.com>
2024-01-18 16:39:35 +0800

    No.008

--- scripts/release-juejin.ts
+++ scripts/release-juejin.ts
@@ -9,9 +9,9 @@ const publich = async () => {
 
   // 从命令行获取掘金 cookie
   // @todo：自动登录掘金获取 cookie
-  const { cookie, confirmed } = await inquirer.prompt([
-    { name: 'cookie', type: 'input', message: 'Input the juejin cookie' },
-    { name: 'confirmed', type: 'confirm', message: 'Confirm to publish?' },
+  const { confirmed, cookie } = await inquirer.prompt([
+    { name: 'confirmed', type: 'confirm', message: chalk.green('\nNeed to publish to Juejin Column?') },
+    { name: 'cookie', type: 'input', message: 'Please input the juejin cookie' },
   ]);
 
   if (confirmed) {
